1a467d6b4c2d51e7b149d8d13a86aa5e44ea2d58
João Carlos Xavier Santos <joao.carlos@conciliadora.local>
2024-08-01 14:16:23 -0300

    implementação final

--- app.js
+++ app.js
@@ -36,6 +36,36 @@ class APIClient {
         }
     }
 
+    async getPagamentos(data_inicial, data_final) {
+        const params = {
+            '$filter': `DataPagamento ge ${data_inicial}T00:00:00-03:00 and DataPagamento le ${data_final}T23:59:59-03:00`
+        };
+        return this._get('ConsultaPagamento', params);
+    }
+
+    static tratarPagamentos(api_valores) {
+        if (!api_valores || !api_valores.value) {
+            console.log("Nenhum pagamento foi retornado ou ocorreu um erro na consulta.");
+            return [];
+        }
+
+        return api_valores.value.map(element => {
+            const valorb = element['ValorBruto'];
+            const valorl = element['ValorLiquido'];
+            const status = valorb < valorl && valorb > 0 ? "Atenção" : "Correto";
+
+            return {
+                Empresa: element['Empresa'],
+                DataPagamento: element['DataPagamento'],
+                Nsu: element['Nsu'],
+                Autorizacao: element['Autorizacao'],
+                ValorBruto: valorb,
+                ValorLiquido: valorl,
+                Status: status
+            };
+        });
+    }
+
     async getPagamentosEDI(data_inicial, data_final) {
         const params = {
             '$filter': `DataPagamento ge ${data_inicial}T00:00:00-03:00 and DataPagamento le ${data_final}T23:59:59-03:00 and AdqId eq 2 and Nsu eq null`
@@ -94,7 +124,7 @@ class APIClient {
     }
 }
 
-app.post('/consultar', async (req, res) => {
+app.post('/consultarduplicidade', async (req, res) => {
     const { data_inicial, data_final, chave_api } = req.body;
     const chaveApiTrimmed = chave_api ? chave_api.trim() : '';
     const apiClient = new APIClient('https://api.conciliadora.com.br/api', { 'Authorization': chaveApiTrimmed });
@@ -104,6 +134,21 @@ app.post('/consultar', async (req, res) => {
         const resultadosprimeiro = APIClient.tratarPagamentosedi(pagamentosedi);
         const pagamentosapi = await apiClient.getPagamentosAPI(data_inicial, data_final);
         const resultados = APIClient.tratarPagamentosapi(pagamentosapi,resultadosprimeiro);
+        res.render('index', { resultados });
+    } catch (error) {
+        res.render('index', { resultados: [] });
+        console.log("erro")
+    }
+});
+
+app.post('/analisar', async (req, res) => {
+    const { data_inicial, data_final, chave_api } = req.body;
+    const chaveApiTrimmed = chave_api ? chave_api.trim() : '';
+    const apiClient = new APIClient('https://api.conciliadora.com.br/api', { 'Authorization': chaveApiTrimmed });
+
+    try {
+        const pagamentos = await apiClient.getPagamentos(data_inicial, data_final);
+        const resultados = APIClient.tratarPagamentos(pagamentos);
         res.render('index', { resultados });
     } catch (error) {
         res.render('index', { resultados: [] });
