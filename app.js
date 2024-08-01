f8a90bfbdaf017df9291ab102b5341e4a9c6b346
João Carlos Xavier Santos <joao.carlos@conciliadora.local>
2024-08-01 16:00:46 -0300

    alterações

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rota principal
 app.get('/', (req, res) => {
+    res.render('index', { resultadosduplicidade: null });
     res.render('index', { resultados: null });
 });
 
